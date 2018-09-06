5ee06de7ccb8109fc5a7d2eee48691b8686d6dd5
XI324337 <p000lgouveia@prservicos.com.br>
2018-09-06 19:07:45 -0300

    Alteração da maneira de leitura do CSV

--- src/main/java/br/com/rsi/capturaSonarEspanha/util/LerArquivoCSV.java
+++ src/main/java/br/com/rsi/capturaSonarEspanha/util/LerArquivoCSV.java
@@ -1,6 +1,7 @@
 package br.com.rsi.capturaSonarEspanha.util;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,50 +10,52 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
 
 public class LerArquivoCSV {
 
 	public static List<MassaCaptura> getInformacoesArquivo() {
-		
-		
-		//Instancio um Properties
+
+		// Instancio um Properties
 		Properties dadosDB = new Properties();
-		
-		//Carrego as informações do arquivo de dados do DB e Sonar para o Properties
+
+		// Carrego as informações do arquivo de dados do DB e Sonar para o
+		// Properties
 		try {
 			dadosDB.load(new FileInputStream("./properties/dados.properties"));
 		} catch (IOException e1) {
-			System.out.println("Erro ao carregar o arquivo dados.properties: "+e1);
+			System.out.println("Erro ao carregar o arquivo dados.properties: " + e1);
 		}
-		
-		String csvArquivo = dadosDB.getProperty("prop.massa");
-		BufferedReader conteudoCSV = null;
-		String linha = "";
-		List<MassaCaptura> massaCapturaCompleta = new ArrayList<>();
-		try {
-			conteudoCSV = new BufferedReader(new FileReader(csvArquivo));
-			while ((linha = conteudoCSV.readLine()) != null) {
-				String nomePainel = linha;
-				MassaCaptura linhaDaMassa = new MassaCaptura(nomePainel);
-				massaCapturaCompleta.add(linhaDaMassa);
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Arquivo não encontrado: \n" + e.getMessage());
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("IndexOutOfBounds: \n" + e.getMessage());
-		} catch (IOException e) {
-			System.out.println("IO Erro: \n" + e.getMessage());
-		} finally {
-			if (conteudoCSV != null) {
-				try {
-					conteudoCSV.close();
-				} catch (IOException e) {
-					System.out.println("IO Erro: \n" + e.getMessage());
-				}
-			}
-		}
-		return massaCapturaCompleta;
 
+		String csvArquivo = dadosDB.getProperty("prop.massa");
+		
+		try {
+			// Carrega a planilha
+			Workbook workbook = null;
+			workbook = Workbook.getWorkbook(new File(csvArquivo));
+
+			// Seleciona a aba do excel
+			Sheet sheet = workbook.getSheet(0);
+			// Numero de linhas com dados do xls
+			int linhas = sheet.getRows();
+			for (int i = 1; i < linhas; i++) {
+				
+				Cell celulaIDPainel = sheet.getCell(1, i); // coluna 1 - ID do Painel
+				Cell celulaCaptura = sheet.getCell(2, i); // coluna 2 - true ou false para captura
+				MassaCaptura linhaMassaCaptura = new MassaCaptura(celulaIDPainel.getContents().toString().trim(),Boolean.getBoolean(celulaCaptura.getContents().toString()));
+				
+			}
+		} catch (NullPointerException e) {
+			System.out.println("Catch 01 ---------------------------------");
+
+		} catch (Exception e) {
+			System.out.println("Catch 02 ---------------------------------");
+		}
+	}
+		
+		
 	}
 
 }
--- src/main/java/br/com/rsi/capturaSonarEspanha/util/MassaCaptura.java
+++ src/main/java/br/com/rsi/capturaSonarEspanha/util/MassaCaptura.java
@@ -3,21 +3,24 @@ package br.com.rsi.capturaSonarEspanha.util;
 public class MassaCaptura {
 
 	private String nomePainel;
+	private Boolean captura;
 
-	MassaCaptura(String painel) {
+	MassaCaptura(String painel, boolean captura) {
 		this.nomePainel = painel;
+		this.captura = captura;
 	}
 
 	public String getPainel() {
 		return nomePainel;
 	}
 
+	public boolean isCaptura() {
+		return captura;
+	}
 
 	@Override
 	public String toString() {
 		return "MassaCaptura [painel=" + nomePainel + "]";
 	}
-	
-	
 
 }
